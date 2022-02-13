17a00ccd8d1a7b52c9cefda84e90749b1d2e66ef
Azizova Irina <i_r_i_n_a_nikitina@mail.ru>
2022-02-13 22:44:39 +0300

    gulp settingas

---
+++ gulpfile.js
@@ -0,0 +1,78 @@
+const gulp = require('gulp');
+const browserSync = require('browser-sync');
+const sass = require('gulp-sass')(require('sass'));
+const cleanCSS = require('gulp-clean-css');
+const rename = require("gulp-rename");
+const autoprefixer = require('gulp-autoprefixer');
+const htmlmin = require('gulp-htmlmin');
+const imagemin = require('gulp-htmlmin');
+const uglify = require('gulp-uglify');
+
+gulp.task( 'server', () => {
+    browserSync({
+        server: {
+            baseDir: 'dist'
+        }
+    });
+    gulp.watch('src/*.html').on('change', browserSync.reload);
+});
+
+gulp.task('styles', () => {
+    return gulp.src('src/sass/**/*.+(scss|sass)')
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(rename({suffix: '.min', prefix: ''}))
+        .pipe(autoprefixer())
+        .pipe(cleanCSS({compatibility: 'ie8'}))        
+        .pipe(gulp.dest('dist/css'))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('html', () => {
+    return gulp.src('src/*.html')
+    .pipe(htmlmin({ collapseWitespace: true}))
+    .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('scripts', () => {
+    return gulp.src('src/js/**/*.js')
+    .pipe(gulp.dest('dist/js'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('fonts', () => {
+    return gulp.src('src/fonts/**/*')
+    .pipe(gulp.dest('dist/fonts'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('icons', () => {
+    return gulp.src('src/icons/**/*')
+    .pipe(gulp.dest('dist/icons'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('images', () => {
+    return gulp.src('src/img/**/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('dist/img'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('compress', function() {
+    return gulp.src('src/js/**/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js'));
+});
+
+
+gulp.task('watch', () => {
+    gulp.watch('src/sass/**/*.+(scss|sass|css)', gulp.parallel('styles'));
+    gulp.watch('src/*.html').on('change', gulp.parallel('html'));
+    gulp.watch('src/js/**/*.js').on('change', gulp.parallel('scripts'));
+    gulp.watch('src/fonts/**/*').on('change', gulp.parallel('fonts'));
+    gulp.watch('src/icons/**/*').on('change', gulp.parallel('icons'));
+    gulp.watch('src/img/**/*').on('change', gulp.parallel('images'));
+});
+
+gulp.task('default', gulp
+    .parallel('server', 'watch', 'styles', 'html', 'scripts', 'fonts', 'icons', 'images', 'compress'));
